92ced1f803582bc0d203fca148ed6ea50b740c16
Khodor Hassoun <khodorhassoun@outlook.com>
2023-02-17 20:16:40 +0200

    Backend: install cors, body-parser and nodemon

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,8 +1,9 @@
 const signup = async (req, res) => {
+  const { name, email, password } = req.body;
   console.log("Sign UP");
-  res.json({ code: 200, req: "Sign UP" });
+  res.json({ name, email, password });
 };
 const signin = async (req, res) => {
-  res.json({ code: 200, req: "Sign IN" });
+  res.json({ code: 200, req: "Sign UP" });
 };
 module.exports = { signup, signin };
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello world");
