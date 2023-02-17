d4b9f51921f519c949fab60d3c5d3d8df32359a1
Khodor Hassoun <khodorhassoun@outlook.com>
2023-02-17 22:29:57 +0200

    Frontend: install axios and tailwind css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  useEffect(() => {
+    axios.get("http://localhost:3002").then((res) => {
+      console.log(res.data);
+    });
+  }, []);
   return (
-    <div className="App">
+    <div className="bg-red-500">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="text-red-800"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
