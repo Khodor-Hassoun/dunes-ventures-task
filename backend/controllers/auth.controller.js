ed649e97c3753478d5ab2f845c4f2abd2ff4c28b
Khodor Hassoun <khodorhassoun@outlook.com>
2023-02-17 20:22:27 +0200

    Backend: connect to database

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,16 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 const signup = async (req, res) => {
   const { name, email, password } = req.body;
-  console.log("Sign UP");
-  res.json({ name, email, password });
+  const user = await prisma.user.create({
+    data: {
+      email: "kkkk",
+      name: "hello",
+      password: "world",
+      all_time_points: 0,
+    },
+  });
+  res.json(user);
 };
 const signin = async (req, res) => {
   res.json({ code: 200, req: "Sign UP" });
