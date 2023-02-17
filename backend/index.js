b4b399cda0446887e1b62c8c37297391dd3c0958
Khodor Hassoun <khodorhassoun@outlook.com>
2023-02-17 18:24:57 +0200

    Backend: install and use dotenv package

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 
@@ -5,6 +6,7 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
-app.listen(3000, () => {
-  console.log("app running on port 3000");
+app.listen(process.env.SERVER_PORT, () => {
+  //   console.log("app running on port 3000");
+  console.log(process.env.SERVER_PORT);
 });
