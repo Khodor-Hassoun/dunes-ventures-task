2359745d1c030f333699577a316330ca0e874b28
Khodor Hassoun <khodorhassoun@outlook.com>
2023-02-17 20:48:53 +0200

    Backend: register user

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,12 +1,47 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
+const bcrypt = require("bcrypt");
+
 const signup = async (req, res) => {
   const { name, email, password } = req.body;
+  let encryptedPassword;
+
+  // Check if all data is there
+  if (!(name && email && password)) {
+    res.status(400).json({ message: "Invalid data" });
+    return;
+  }
+
+  //   Check if user email already exists
+  const testEmail = await prisma.user.findFirst({
+    where: {
+      email: email,
+    },
+  });
+  if (testEmail) {
+    res.status(400).json({ message: "Email already exists" });
+    return;
+  }
+
+  //   Validate password and tranform to hash
+  //   if (password.length >= 8) {
+  //     encryptedPassword = await bcrypt.hash(password, 10);
+  //   } else {
+  //     res.status(400).json({ message: "password not strong" });
+  //     return;
+  //   }
+  if (password) {
+    encryptedPassword = await bcrypt.hash(password, 10);
+  } else {
+    res.status(400).json({ message: "password not strong" });
+    return;
+  }
+
   const user = await prisma.user.create({
     data: {
-      email: "kkkk",
-      name: "hello",
-      password: "world",
+      email: email,
+      name: name,
+      password: encryptedPassword,
       all_time_points: 0,
     },
   });
