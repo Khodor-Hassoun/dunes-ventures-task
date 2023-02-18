db0fa08cf1f4ce6198eb48270bdf61e3b0b7ec3a
Khodor Hassoun <khodorhassoun@outlook.com>
2023-02-18 21:33:30 +0200

    Frontend: add validation to signin page

--- frontend/src/components/SignInForm.jsx
+++ frontend/src/components/SignInForm.jsx
@@ -1,20 +1,22 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import signIn from "../api/signin";
 
 export default function SignInForm() {
-
+    const [failedSignIn, setFailedSignIn] = useState(false)
     const navigate = useNavigate()
-    const { register, handleSubmit } = useForm()
+    const { register, handleSubmit, formState: { errors } } = useForm()
     function submitHandler(data) {
         signIn(data).then(res => {
             console.log(res)
             navigate("/game")
         }).catch(e => {
             console.log(e)
+            setFailedSignIn(true)
         })
     }
-
+    console.log(errors)
     return (
         <form className="flex flex-col text-black [&>*]:my-2 w-full"
             onSubmit={handleSubmit(submitHandler)}
@@ -23,22 +25,49 @@ export default function SignInForm() {
             <div className="[&>*]:my-4">
                 <div className="flex flex-col box-border w-full">
                     <label htmlFor="email" className="text-lg">Email</label>
-                    <input name="email" type="text" className="border-2 border-black box-border py-2 rounded"
-                        {...register("email")}
+                    <input name="email" type="text"
+                        className={"border-2 border-black focus:border-red-500 box-border py-2 rounded"}
+                        {...register("email", {
+                            pattern: {
+                                value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                                message: "Pattern not available"
+                            },
+                            required: {
+                                value: true,
+                                message: "Missing Email"
+                            }
+
+                        })}
                     ></input>
+                    {
+                        errors.email ?
+                            <p className="text-xs text-red-500">Email Invalid</p>
+                            : ""
+                    }
                 </div>
 
                 {/* password */}
                 <div className="flex flex-col w-full">
                     <label htmlFor="password" className="text-lg p-0.5">Password</label>
                     <input name="password" type="password" className="border-2 border-black box-border py-2 rounded"
-                        {...register("password")}
+                        {...register("password", {
+                            required: "Password cannot be empty",
+                            minLength: {
+                                value: 3,
+                                message: "Password too short"
+                            },
+
+                        })}
                     ></input>
+                    {
+                        errors.password ?
+                            <p className="text-xs text-red-500">Password Invalid</p>
+                            : ""
+                    }
                 </div>
 
             </div>
-            {/* Email */}
-
+            {failedSignIn && <p className="text-xs text-red-500">Something went wrong</p>}
             <button className="w-full bg-ming text-[#EEEEEE] text-lg py-2 rounded">Sign In</button>
         </form>
     )
--- frontend/src/pages/SignIn/SignIn.jsx
+++ frontend/src/pages/SignIn/SignIn.jsx
@@ -1,4 +1,5 @@
 import SignInForm from "../../components/SignInForm";
+import { Link } from "react-router-dom";
 
 export default function SignIn() {
     return <div
@@ -8,9 +9,13 @@ export default function SignIn() {
 
         </div> */}
         <div className="w-5/12 flex">
-            <div className="bg-white text-black flex flex-col box-border pb-10 px-5 rounded-md justify-center w-10/12">
+            <div className="bg-white text-black flex flex-col box-border pb-10 px-5 rounded-md justify-center w-full">
                 <h2 className="pb-4 pt-12 flex justify-start text-3xl">Lets play...</h2>
                 <SignInForm />
+                <span className="my-4 flex flex-col justify-center items-center">
+                    Don't have an account?
+                    <span className="text-tangerine"> <Link to="/signup">Register here</Link></span>
+                </span>
             </div>
         </div>
 
